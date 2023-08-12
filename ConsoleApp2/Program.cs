ece8b49736cb37c339d37fcc1ae1bde40145529d
polten0 <alexey.mikryukov@mail.ru>
2023-08-12 12:22:57 +0300

    helloCommit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,6 +2,9 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Как тебя зовут? Напиши: ");
+        string input = Console.ReadLine();
+        Console.WriteLine("Привет, " + input);
+
     }
 }
